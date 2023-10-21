693d22cee977a83852ffb5f1f86fa7eb573ff990
Samuel Jimenez <samuelelias13@hotmail.com>
2023-10-21 18:37:41 -0400

    Creating Entities

---
+++ SocialNetwork.Core.Domain/Entites/Friends.cs
@@ -0,0 +1,21 @@
+﻿using SocialNetwork.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Core.Domain.Entites
+{
+    public class Friends : BaseEntity
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+
+
+        //Navigation Propierties
+        public int UserID { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ SocialNetwork.Core.Domain/Entites/Publications.cs
@@ -0,0 +1,18 @@
+﻿using SocialNetwork.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Core.Domain.Entites
+{
+    public class Publications : BaseEntity
+    {
+        public string Content { get; set; }
+
+        //Navigation propierties 
+        public int UserID { get; set; }
+        public User User { get; set; }
+    }
+}
